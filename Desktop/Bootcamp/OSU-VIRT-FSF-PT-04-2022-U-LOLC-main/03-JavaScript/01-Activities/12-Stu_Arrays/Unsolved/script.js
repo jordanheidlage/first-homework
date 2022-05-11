821b076e2878762ec69a05b314f70c43046db992
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-05-11 19:17:37 -0400

    updated array

---
+++ Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -0,0 +1,7 @@
+var names = ["Pete", "Jim", "Steven", "Doug"];
+
+console.log(names.length);
+console.log("Welcome to the class " + names[0]);
+console.log("Welcome to the class " + names[1]);
+console.log("Welcome to the class " + names[2]);
+console.log("Welcome to the class " + names[3]);
