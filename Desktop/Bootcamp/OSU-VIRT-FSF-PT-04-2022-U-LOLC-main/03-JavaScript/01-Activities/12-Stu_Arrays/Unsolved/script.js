8989d13a41ad1b6daa491b6c9b0a61ddbb3d9951
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-05-11 19:20:15 -0400

    updated

--- Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -5,3 +5,7 @@ console.log("Welcome to the class " + names[0]);
 console.log("Welcome to the class " + names[1]);
 console.log("Welcome to the class " + names[2]);
 console.log("Welcome to the class " + names[3]);
+
+names[0]="Jordan"
+
+console.log("Welcome to the class")
