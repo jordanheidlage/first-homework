b663af3b50580f877a0c2be6ce5e480e844cf754
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-05-11 21:18:35 -0400

    variable stuff

--- Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,4 @@
 //WRITE YOUR CODE BELOW
+var students=["Kim","Marcus", "Texas Joe", "Kevin", "Annie"];
+
+for (var i)
--- Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/18-Stu_Scope/Unsolved/script.js
+++ Desktop/Bootcamp/OSU-VIRT-FSF-PT-04-2022-U-LOLC-main/03-JavaScript/01-Activities/18-Stu_Scope/Unsolved/script.js
@@ -1,4 +1,5 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+var shout = "shout";
 function justShout() {
   console.log(shout + ", " + shout);
   return;
@@ -13,6 +14,7 @@ justShout();
 shoutItAllOut();
 
 // TODO: Declare variable 'animal' with the value 'Tigers' so it is only available to the 'sayTigers' function
+
 function sayLions() {
   var animal = "Lions"; 
   console.log(animal);
@@ -20,6 +22,7 @@ function sayLions() {
 }
 
 function sayTigers() {
+  var animal = "Tigers";
   console.log("and " + animal + " and "); 
   return;
 }
